d61de17ed1ee7ba16ce1fa975d3ad4ba12bd35fd
Mark Garrett <srcmark@garetech.com.au>
2011-04-14 02:33:22 +0000

    optional file include

--- src/mail_utils.h
+++ src/mail_utils.h
@@ -1,7 +1,7 @@
 #ifndef __MAIL_UTILS_H__
 #define	__MAIL_UTILS_H__
 /*
- * @(#) $Id: mail_utils.h,v 1.1 2010/11/16 04:04:00 root Exp mark $
+ * @(#) $Id: mail_utils.h,v 1.2 2011/04/11 03:53:53 mark Exp mark $
  *
  *  mail_utils.h
  *  fmtbckrep_xcode
@@ -10,6 +10,9 @@
  *  Copyright 2009 Garetech Computer Solutions. All rights reserved.
  *
  *  $Log: mail_utils.h,v $
+ *  Revision 1.2  2011/04/11 03:53:53  mark
+ *  add include log stuff
+ *
  *  Revision 1.1  2010/11/16 04:04:00  root
  *  Initial revision
  *
@@ -18,7 +21,7 @@
 #include "fileio.h"
 #define _PATH_SENDMAIL	"/usr/sbin/sendmail"
 
-int mailto(files_t **mailfd_head, char *mailtoaddr, char *subject, int dbgflag);
+int mailto(files_t **mailfd_head, char *mailtoaddr, char *subject, int dbgflag, int include_flg);
 int	attach_mime_file(fio_t **mailfd_ptr, fio_t *mailtmp, char *bounary_str);
 
 #endif /* __MAIL_UTILS_H__ */
