b29c5d6d66656b71696dbc49a74f2e0ae19febdd
Mark Garrett <srcmark@garetech.com.au>
2013-01-21 16:46:45 +0000

    MG add missing recorddate to vol_t structure reformating

--- src/data_structures.h
+++ src/data_structures.h
@@ -1,13 +1,16 @@
 #ifndef __DATA_STRUCTURE_H__
 #define __DATA_STRUCTURE_H__
 /*
- // @(#) $Id: data_structures.h,v 1.2 2011/04/11 03:52:00 mark Exp mark $
+ // @(#) $Id: data_structures.h,v 1.3 2013/01/20 10:06:26 mark Exp mark $
  //  data_structures.h
  //  blib
  //
  //  Created by mark on 18/10/2010.
  //  Copyright (c) 2010 Garetech Computer Solutions. All rights reserved.
  // $Log: data_structures.h,v $
+ // Revision 1.3  2013/01/20 10:06:26  mark
+ // MG changes for time_t to blib_tim_t
+ //
  // Revision 1.2  2011/04/11 03:52:00  mark
  // add val_t
  //
@@ -80,15 +83,15 @@ typedef struct {
 } dbh_t;
 
 // if these change remember to review the format statment in do_cmd_report()
-#define		LABEL_SIZ		9
-#define		MEDIA_NAM_SIZ		9
-#define		STATE_SIZ		1
-#define		DESC_SIZ		256
-#define		NODE_SIZ		256
-#define		OBJ_NAME_SIZ		1024
-#define		GROUPNAM_SIZ		9
-#define		LOCNAM_SIZ		64
-#define		ERRMSG_SIZ		256
+#define		LABEL_SIZ		     9
+#define		MEDIA_NAM_SIZ		 9
+#define		STATE_SIZ		     1
+#define		DESC_SIZ		   256
+#define		NODE_SIZ		   256
+#define		OBJ_NAME_SIZ	  1024
+#define		GROUPNAM_SIZ	 	 9
+#define		LOCNAM_SIZ		    64
+#define		ERRMSG_SIZ		   256
 
 #define		LABEL_SIZ_STR		"9"
 #define		MEDIA_NAM_SIZ_STR	"9"
@@ -143,6 +146,7 @@ typedef struct {
 
 typedef struct backups_s backups_t;
 struct backups_s {
+    // bck_id|node|start|end|expiredate|desc
     bckid_t     bck_id;		// backup id of this backup set
     node_t      node;		// backup for node
     blib_tim_t	start;
@@ -153,6 +157,7 @@ struct backups_s {
 
 typedef struct vol_s vol_t;
 struct vol_s {
+    // bck_id|label|state|media|usage|groupname|location|librarydate|recorddate|offsitedate
     bckid_t     bck_id;			// what ever was written onto the tape last
     blabel_t	label;			// the volume label eg ABC123D
     state_t     state;
@@ -161,14 +166,16 @@ struct vol_s {
     groupname_t	groupname;
     location_t	location;
     blib_tim_t	librarydate;
+    blib_tim_t  recorddate;
     blib_tim_t	offsitedate;
 };
 
 typedef struct bckobj_s	bckobj_t;
 struct bckobj_s {
-    bckid_t	bck_id;
+    // bck_id|objname|obj_instance|start|end|size
+    bckid_t	    bck_id;
     objname_t	objname;
-    objid_t	obj_instance;
+    objid_t	    obj_instance;
     blib_tim_t	start;
     blib_tim_t	end;
     bcount_t	size;
@@ -176,9 +183,10 @@ struct bckobj_s {
 
 typedef struct vol_obj_s vol_obj_t;	// link table between volumes and backup objects
 struct vol_obj_s {
-    bckid_t	bck_id;		// link bckobj_t.bck_id or backups_t.bck_id
+    // bck_id|objname|obj_instance|label|fileno|start|end|size
+    bckid_t	    bck_id;		// link bckobj_t.bck_id or backups_t.bck_id
     objname_t	objname;	// link bckobj_t.objname
-    objid_t	obj_instance;
+    objid_t	    obj_instance;
     blabel_t	label;		// link vol_t.label
     uint32_t	fileno;		// the file number on the volume
     blib_tim_t	start;
@@ -188,10 +196,11 @@ struct vol_obj_s {
 
 typedef struct bck_errors_s bck_errors_t;
 struct bck_errors_s {
-    bckid_t	bck_id;
+    // bck_id|label|objname|obj_instance|errtime|errmsg
+    bckid_t	    bck_id;
     blabel_t	label;
     objname_t	objname;
-    objid_t	obj_instance;
+    objid_t	    obj_instance;
     blib_tim_t	errtime;
     errmsg_t	errmsg;
 };
@@ -199,29 +208,29 @@ struct bck_errors_s {
 
 typedef struct qual_s qual_t;
 struct qual_s {
-    bckid_t	bck_id;
-    desc_t	desc;
-    blib_tim_t	end;
-    int		errcount;
-    errmsg_t	errmsg;
-    blib_tim_t	expiredate;
-    int		filecount;
-    int		fileno;
+    bckid_t	    bck_id;
+    desc_t	    desc;
+    blib_tim_t  end;
+    int		    errcount;
+    errmsg_t    errmsg;
+    blib_tim_t  expiredate;
+    int		    filecount;
+    int		    fileno;
     groupname_t	groupname;
     blabel_t	label;
     blib_tim_t	librarydate;
     location_t	location;
-    media_t	media;
-    node_t	node;
+    media_t	    media;
+    node_t	    node;
     objname_t	objname;
-    objid_t	obj_instance;
+    objid_t	    obj_instance;
     blib_tim_t	offsitedate;
     blib_tim_t	recorddate;
     bcount_t	size;
     blib_tim_t	start;
-    state_t	state;
-    char	*stylesheet;
-    int		usage;
+    state_t	    state;
+    char	    *stylesheet;
+    int		    usage;
     blib_tim_t	since;
     blib_tim_t	until;
 };
